9659bb1a7f6e81160cc537d9dafb4c1875c0e41f
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-03-06 16:23:32 -0500

    fix(generator): No function wrapper

--- generators/app/templates/test-bdd.js
+++ generators/app/templates/test-bdd.js
@@ -1,11 +1,9 @@
-(function () {
-  'use strict';
+'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+describe('Give it some context', function () {
+  describe('maybe a bit more context here', function () {
+    it('should run here few assertions', function () {
 
-      });
     });
   });
-})();
+});
--- generators/app/templates/test-tdd.js
+++ generators/app/templates/test-tdd.js
@@ -1,11 +1,9 @@
-(function () {
-  'use strict';
+'use strict';
 
-  suite('Give it some context', function () {
-    suite('maybe a bit more context here', function () {
-      test('should run here few assertions', function () {
+suite('Give it some context', function () {
+  suite('maybe a bit more context here', function () {
+    test('should run here few assertions', function () {
 
-      });
     });
   });
-})();
+});
